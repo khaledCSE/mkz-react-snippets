a46a55272ff092546b392f9563634a982f8c3c21
khaledCSE <sporshozaman@gmail.com>
2021-03-18 13:22:52 +0600

    Fixed First Working Component

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { followZoom } from './followZoom';
 
 export const FollowZoomImage = ({ class_name, imagePath }) => {
+    const defaultStyles = {
+        background: `url(${imagePath})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    };
     const handleZoom = (e) => followZoom(e.target);
     return (
         <div
             className={class_name}
-            style={{ background: `url(${imagePath})` }}
+            style={defaultStyles}
             onMouseMove={handleZoom}
         ></div>
     );
